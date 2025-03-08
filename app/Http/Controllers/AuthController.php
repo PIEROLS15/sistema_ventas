8af98ee1fb670100d46751a3b70868d9e903b5b9
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2025-03-08 15:06:02 -0500

    fix(User): Mejoras en la validación de registro de usuarios

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,37 +8,52 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    
     // Registro de usuario
     public function register(Request $request)
     {
-        $request->validate([
-            'nombre' => ['required', 'string', 'max:255', 'not_regex:/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/'],
-            'apellido' => ['required', 'string', 'max:255', 'not_regex:/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/'],
-            'correo' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-            'role_id' => 'nullable|exists:roles,id',
-        ],[
-            'nombre.not_regex' => 'El nombre no puede contener números ni caracteres especiales.',
-            'apellido.not_regex' => 'El apellido no puede contener números ni caracteres especiales.',
-        ]);
-    
-        $user = User::create([
-            'nombre' => $request->nombre,
-            'apellido' => $request->apellido,
-            'correo' => $request->correo,
-            'password' => bcrypt($request->password),
-            'role_id' => $request->role_id ?? 2,
-        ]);
+        try {
+            $request->validate([
+                'nombre' => ['required', 'string', 'max:255', 'regex:/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/'],
+                'apellido' => ['required', 'string', 'max:255', 'regex:/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/'],
+                'correo' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+                'role_id' => 'nullable|exists:roles,id',
+            ], [
+                'nombre.required' => 'Los nombres son obligatorios.',
+                'nombre.regex' => 'El nombre no puede tener números ni caracteres especiales.',
+                'apellido.required' => 'Los apellidos son obligatorios.',
+                'apellido.regex' => 'El apellido no puede tener números ni caracteres especiales.',
+                'correo.required' => 'El correo electrónico es obligatorio.',
+                'correo.email' => 'Ingresa un correo electrónico válido.',
+                'correo.unique' => 'El correo ingresado ya está registrado. Por favor, intenta con otro.',
+                'password.required' => 'La contraseña es obligatoria.',
+                'password.min' => 'La contraseña debe tener al menos 6 caracteres.',
+                'password.confirmed' => 'Las contraseñas no coinciden.',
+                ]);
 
-        $token = $user->createToken('authToken')->plainTextToken;
+            $user = User::create([
+                'nombre' => $request->nombre,
+                'apellido' => $request->apellido,
+                'correo' => $request->correo,
+                'password' => Hash::make($request->password),
+                'role_id' => $request->role_id ?? 2,
+            ]);
 
-        return response()->json([
-            'user' => $user->load('role'),
-            'token' => $token,
-            'role' => $user->role->name
-        ], 201);
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'user' => $user->load('role'),
+                'token' => $token,
+                'role' => $user->role->name
+            ], 201);
+
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'message' => $e->validator->errors()->first()
+            ], 422);
+        }
     }
-
 
     // Inicio de sesión
     public function login(Request $request)
